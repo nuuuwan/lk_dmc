de02f51537278896e5f13df9c5f5cc33f0c033c4
nuuuwan <nuuuwan@gmail.com>
2025-11-28 10:20:04 +0530

    Implement data scraping logic in RiverWaterLevelAndFloodWarnings class

--- src/dmc/RiverWaterLevelAndFloodWarnings.py
+++ src/dmc/RiverWaterLevelAndFloodWarnings.py
@@ -31,3 +31,16 @@ class RiverWaterLevelAndFloodWarnings(AbstractDMCPDFDoc):
             report_type_id=6,
             lang="en",
         )
+
+    def scrape_extended_data_for_doc(self):
+        if not self.has_pdf:
+            self.download_pdf()
+
+        if self.has_pdf and not self.has_blocks:
+            self.extract_blocks()
+
+        if self.has_pdf and not self.has_tabular:
+            self.extract_tabular()
+
+        if self.has_blocks and not self.has_text:
+            self.extract_text()
